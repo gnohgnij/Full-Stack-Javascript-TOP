377107a38568d7195dcc8c930564cf5832292c46
Chong Jing Hong <congjinghong@gmail.com>
2021-01-05 17:23:39 +0800

    add new books function and display books function

--- Library/script.js
+++ Library/script.js
@@ -8,27 +8,65 @@ function Book(title, author, pages, read){
 }
 
 function addBookToLibrary(){
-    // let title = prompt("Name of book to add: ");
-    // let author = prompt("Name of author: ");
-    // let pages = prompt("Number of pages: ");
-    // let read = prompt("Have you finished reading? ");
-    // const newBook = new Book(title, author, pages, read);
-    // myLibrary.push(newBook);
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const pages = document.getElementById('pages').value;
+    let read;
+    if(document.getElementById('read').checked){
+        read = document.getElementById('read').value;
+    }
+    else if(document.getElementById('not-read').checked){
+        read = document.getElementById('not-read').value;
+    }
+
+    const newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
 }
 
 function displayBooks(){
+    const allBooks = document.querySelector('#all-books');
+
+    const bookItems = document.querySelectorAll('.book-item');
+    bookItems.forEach(book => book.remove());   
+
     for(let i=0; i<myLibrary.length; i++){
-        console.log(myLibrary[i]);
+        const newDiv = document.createElement('div');
+        newDiv.classList.add('book-item');
+        newDiv.innerHTML = 'Title: ' + myLibrary[i]["title"] + '<br>';
+        newDiv.innerHTML += 'Author: ' + myLibrary[i]["author"] + '<br>';
+        newDiv.innerHTML += 'Pages: ' + myLibrary[i]["pages"] + '<br>';
+        newDiv.innerHTML += 'Read: ' + myLibrary[i]["read"];
+        allBooks.appendChild(newDiv);
     }
+    const books = document.querySelectorAll('.book-item');
+    books.forEach(book => setStyles(book));
 }
 
-const addBookBtn = document.querySelector("#add");
-addBookBtn.addEventListener("click", function(){
+function setStyles(book){
+    book.style.color = 'rgb(169, 63, 85)';
+    book.style.fontSize = '0.75rem';
+    book.style.textAlign = 'center';
+    book.style.border = '2px solid rgb(169, 63, 85)';
+}
+
+const newBookBtn = document.querySelector("#new");
+newBookBtn.addEventListener("click", function(){
     document.querySelector("#form-container").style.display = "block";
-    addBookToLibrary();
 });
 
-const displayBooksBtn = document.querySelector("#display");
-displayBooksBtn.addEventListener("click", function(){
+const cancelBtn = document.querySelector("#cancel");
+cancelBtn.addEventListener("click", function(){
+    document.querySelector("#form-container").style.display = "none";
+})
+
+const submitBtn = document.querySelector('#submit');
+submitBtn.addEventListener("click", function(){
+    addBookToLibrary();
+    document.querySelector("#form-container").style.display = "none";
     displayBooks();
 })
+
+// const displayBooksBtn = document.querySelector("#display");
+// displayBooksBtn.addEventListener("click", function(){
+//     displayBooks();
+// })
