2aa0b266f8a3da226ebeb0983675de8d53a49b43
Chong Jing Hong <congjinghong@gmail.com>
2021-01-06 18:37:36 +0800

    DONEZO

--- Library/script.js
+++ Library/script.js
@@ -30,43 +30,81 @@ function displayBooks(){
     bookItems.forEach(book => book.remove());   
 
     for(let i=0; i<myLibrary.length; i++){
-        const newDiv = document.createElement('div');
-        newDiv.classList.add('book-item');
-        newDiv.innerHTML = 'Title: ' + myLibrary[i]["title"] + '<br>';
-        newDiv.innerHTML += 'Author: ' + myLibrary[i]["author"] + '<br>';
-        newDiv.innerHTML += 'Pages: ' + myLibrary[i]["pages"] + '<br>';
-        newDiv.innerHTML += 'Read: ' + myLibrary[i]["read"];
-        allBooks.appendChild(newDiv);
+        const bookDiv = createBook(myLibrary[i]);
+        setStyles(bookDiv);
+        allBooks.appendChild(bookDiv);
     }
-    const books = document.querySelectorAll('.book-item');
-    books.forEach(book => setStyles(book));
+}
+
+function createBook(book){
+    const bookDiv = document.createElement('div');
+    bookDiv.classList.add('book-item');
+    bookDiv.setAttribute('id', myLibrary.indexOf(book));
+    bookDiv.innerHTML = '<i class="fas fa-book fa-4x"></i>';
+    bookDiv.innerHTML += '<div class="info">' + book["title"] + 
+            '<br>' + book["author"] + '<br>' + 
+            book["pages"] + ' pages';
+    const removeBtn = document.createElement('button');
+    removeBtn.setAttribute('id', 'remove'+myLibrary.indexOf(book));
+    removeBtn.addEventListener('click', function(){
+        myLibrary.splice(myLibrary.indexOf(book), 1);
+        displayBooks();
+    })
+    removeBtn.innerHTML = 'Remove';
+    removeBtn.style.backgroundColor = '#AEB7B3';
+    removeBtn.style.border = '1px solid';
+    removeBtn.style.color = '#160C28';
+    removeBtn.style.fontFamily = 'Bree Serif, serif';
+    removeBtn.style.display = 'block';
+    removeBtn.style.margin = '10px auto';
+    bookDiv.appendChild(removeBtn);
+
+    const readBtn = document.createElement('button');
+    readBtn.setAttribute('id', 'read'+myLibrary.indexOf(book));
+    readBtn.innerHTML = book['read'];
+    readBtn.style.backgroundColor = '#AEB7B3';
+    readBtn.style.border = '1px solid';
+    readBtn.style.color = '#160C28';
+    readBtn.style.fontFamily = 'Bree Serif, serif';
+    readBtn.style.display = 'block';
+    readBtn.style.margin = '10px auto';
+    readBtn.addEventListener('click', function(){
+        if(book['read'] == 'Read'){
+            book['read'] = 'Not Read';
+        }
+        else if(book['read'] == 'Not Read'){
+            book['read'] = 'Read';
+        }
+        displayBooks();
+    })
+    bookDiv.appendChild(readBtn);
+
+
+    return bookDiv;
 }
 
 function setStyles(book){
-    book.style.color = 'rgb(169, 63, 85)';
-    book.style.fontSize = '0.75rem';
+    book.style.backgroundColor = '#E1EFE6';
+    book.style.fontSize = '1rem';
     book.style.textAlign = 'center';
-    book.style.border = '2px solid rgb(169, 63, 85)';
+    book.style.padding = '10px';
+    book.style.boxShadow = '0 4px 8px 0';
+    book.style.borderRadius = '5px';
 }
 
 const newBookBtn = document.querySelector("#new");
 newBookBtn.addEventListener("click", function(){
-    document.querySelector("#form-container").style.display = "block";
+    document.querySelector("form").style.display = "block";
 });
 
 const cancelBtn = document.querySelector("#cancel");
 cancelBtn.addEventListener("click", function(){
-    document.querySelector("#form-container").style.display = "none";
+    document.querySelector("form").style.display = "none";
 })
 
 const submitBtn = document.querySelector('#submit');
 submitBtn.addEventListener("click", function(){
     addBookToLibrary();
-    document.querySelector("#form-container").style.display = "none";
+    document.querySelector("form").style.display = "none";
     displayBooks();
 })
-
-// const displayBooksBtn = document.querySelector("#display");
-// displayBooksBtn.addEventListener("click", function(){
-//     displayBooks();
-// })
